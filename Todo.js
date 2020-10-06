a22e7843ce7ecd858ad85c424b6e47aec2100443
Paul <isakura313@gmail.com>
2020-10-06 21:26:51 +0300

    first commit of 16 lesson

---
+++ Todo.js
@@ -0,0 +1,15 @@
+export class Todo{
+    /*
+        priority - важность дела
+        text - контент, содержимое
+        dt -  дата создания дела
+        dl - deadline
+     */
+    constructor(priority, text, dl) {
+        this.priority = priority;
+        this.text = text;
+        this.dt = Date.now();
+        this.dl = dl;
+    }
+}
+
---
+++ code.js
@@ -0,0 +1,4 @@
+import Todo from './Todo';
+
+let todo =  new Todo(1, 'дело', '15:10')
+console.log(todo)
