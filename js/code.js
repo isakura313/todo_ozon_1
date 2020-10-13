860f8a053891670f7343651f6e451d5e13dcb2ff
Paul <isakura313@gmail.com>
2020-10-13 22:26:45 +0300

    asdasd

--- js/code.js
+++ js/code.js
@@ -1,4 +1,40 @@
 import Todo from './Todo';
 
-let todo = new Todo(1, 'дело', '15:10')
-console.log(todo)
+const field = document.querySelector(".input")
+let select = document.querySelector("select");
+
+/*
+Функция добавления дела, котора вызвает функцию отрисовки
+и добавляет дело в LocalStorage
+И не забывает обнулять значения
+ */
+function addTask(){
+    let content = field.value;
+    if(content == ""){
+        return;
+    }
+    let todo = new Todo(select.value, content, '20/10/20')
+    // дальше у нас будет сохрание в LocalStorage
+}
+
+//назначение листенеров на нашу кнопку
+
+
+
+// функция отрисовки нашего приложения, когда оно включается
+//данные берутся из localStorage
+
+
+
+/*
+Универсальная функция отрисовки, которая у нас вставляет todo в DOM
+ */
+function GenerateDom(){
+
+}
+
+// обработчики удаления дела
+
+
+// обработчики редактирования дела
+
