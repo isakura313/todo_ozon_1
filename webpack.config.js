b41a15c3531b21ca0590dc0887b603122993c9a2
alexandrYun <alexandryun@mail.ru>
2020-10-18 16:10:45 +0300

    добавлен browsersync

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
-    mode: "development",
-    watch: true,
-    entry: './js/code.js',
-    devtool: "inline-cheap-source-map",
-    watchOptions: {
-        ignored: ["node_modules/**"],
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
-    },
+  mode: "development",
+  watch: true,
+  entry: './js/code.js',
+  devtool: "inline-cheap-source-map",
+  watchOptions: {
+    ignored: ["node_modules/**"],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js'
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['public'] }
+    })
+  ]
 };
